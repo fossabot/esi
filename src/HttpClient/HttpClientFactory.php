6f0b66e7d8fd8fa915b15b22b4dd4099245c58ab
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-14 22:48:15 +0000

    Early client structure with initial plugins

---
+++ src/HttpClient/HttpClientFactory.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace AGrimes94\Esi\HttpClient;
+
+use Http\Client\Common\HttpMethodsClient;
+use Http\Client\Common\Plugin;
+use Http\Client\Common\PluginClient;
+use Http\Client\HttpClient;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
+use Http\Discovery\StreamFactoryDiscovery;
+use Http\Message\MessageFactory;
+use Http\Message\RequestFactory;
+use Http\Message\StreamFactory;
+
+/**
+ * HttpClientFactory constructs HttpClient/PluginClient with specified options
+ *
+ * @author Anthony Grimes <contact@anthonygrimes.co.uk>
+ */
+class HttpClientFactory
+{
+    /**
+     * The object that sends HTTP messages.
+     *
+     * @var HttpClient
+     */
+    private $httpClient;
+
+    /**
+     * A HTTP client with all our plugins.
+     *
+     * @var PluginClient
+     */
+    private $pluginClient;
+
+    /**
+     * Request object.
+     *
+     * @var MessageFactory
+     */
+    private $requestFactory;
+
+    /**
+     * Stream object.
+     *
+     * @var StreamFactory
+     */
+    private $streamFactory;
+
+    /**
+     * True if we should create a new Plugin client at next request.
+     *
+     * @var bool
+     */
+    private $httpClientModified = true;
+
+    /**
+     * Plugin array to be used in constructing plugin chain.
+     *
+     * @var Plugin[]
+     */
+    private $plugins = [];
+
+    /**
+     * @param HttpClient $httpClient
+     * @param RequestFactory $requestFactory
+     * @param StreamFactory $streamFactory
+     */
+    public function __construct(
+        HttpClient $httpClient = null,
+        RequestFactory $requestFactory = null,
+        StreamFactory $streamFactory = null
+    )
+    {
+        $this->httpClient = $httpClient ?: HttpClientDiscovery::find();
+        $this->requestFactory = $requestFactory ?: MessageFactoryDiscovery::find();
+        $this->streamFactory = $streamFactory ?: StreamFactoryDiscovery::find();
+    }
+
+    /**
+     * Retrieve HttpMethodsClient instance.
+     *
+     * @return HttpMethodsClient
+     */
+    public function getHttpClient()
+    {
+        if ($this->httpClientModified) {
+            $this->httpClientModified = false;
+            $this->pluginClient = new HttpMethodsClient(
+                new PluginClient($this->httpClient, $this->plugins),
+                $this->requestFactory
+            );
+        }
+        return $this->pluginClient;
+    }
+
+    /**
+     * Add a new plugin to the end of the plugin chain.
+     *
+     * @param Plugin $plugin
+     */
+    public function addPlugin(Plugin $plugin)
+    {
+        $this->plugins[] = $plugin;
+        $this->httpClientModified = true;
+    }
+
+    /**
+     * Remove a plugin by its fully qualified class name (FQCN).
+     *
+     * @param string $fqcn
+     */
+    public function removePlugin($fqcn)
+    {
+        foreach ($this->plugins as $idx => $plugin) {
+            if ($plugin instanceof $fqcn) {
+                unset($this->plugins[$idx]);
+                $this->httpClientModified = true;
+            }
+        }
+    }
+}
---
+++ src/HttpClient/Plugin/ApiVersion.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AGrimes94\Esi\HttpClient\Plugin;
+
+use Http\Client\Common\Plugin;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Prefix path with desired api version to query
+ *
+ * @author Anthony Grimes <contact@anthonygrimes.co.uk>
+ */
+class ApiVersion implements Plugin
+{
+    /**
+     * @var string
+     */
+    private $apiVersion;
+
+    /**
+     * @param string $apiVersion
+     */
+    public function __construct($apiVersion = 'latest')
+    {
+        $this->apiVersion = $apiVersion;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleRequest(RequestInterface $request, callable $next, callable $first)
+    {
+        $uri = $request->getUri();
+
+        if (substr($uri->getPath(), 0, 8) !== '/latest/') {
+            $request = $request->withUri($uri->withPath($this->apiVersion . $uri->getPath()));
+        }
+
+        return $next($request);
+    }
+}
