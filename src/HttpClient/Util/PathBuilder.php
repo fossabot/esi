33aee921da02162375b0412cdd89c39eafb62ec8
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-15 20:48:07 +0000

    Updated PathBuilder to build new paths given params

--- src/HttpClient/Util/PathBuilder.php
+++ src/HttpClient/Util/PathBuilder.php
@@ -9,4 +9,25 @@ namespace AGrimes94\Esi\HttpClient\Util;
  */
 final class PathBuilder
 {
+    /**
+     * Builds a viable path given a "unfull" path
+     *
+     * given:   '/characters/{character_id}/skills/', ['character_id' => 1234,]
+     * returns: '/characters/1234/skills/'
+     *
+     * @param string $path
+     * @param array $pathData
+     *
+     * @return string
+     */
+    public static function build(string $path, array $pathData): string
+    {
+        if (preg_match_all('/{+(.*?)}/', $path, $matches)) {
+            foreach ($matches[1] as $match) {
+                $path = str_replace('{' . $match . '}', $pathData[$match], $path);
+            }
+        }
+
+        return $path;
+    }
 }
