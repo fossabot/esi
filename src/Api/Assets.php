b796fa981208a29d0aa9c66e081ca4fffad668e0
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-17 21:14:24 +0000

    Add assets api endpoints

--- src/Api/Assets.php
+++ src/Api/Assets.php
@@ -9,5 +9,109 @@ namespace AGrimes94\Esi\Api;
  */
 class Assets extends AbstractApi
 {
+    /**
+     * Endpoint: /characters/{character_id}/assets/
+     *
+     * Return a list of the characters assets.
+     *
+     * @param int $characterId
+     * @param int $page
+     *
+     * @return mixed
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function getCharacterAssets(int $characterId, int $page = 1)
+    {
+        return $this->get('/characters/' . $this->encodePath($characterId) . '/assets/', $this->paginateQuery($page));
+    }
 
+    /**
+     * Endpoint: /characters/{character_id}/assets/locations/
+     *
+     * Return locations for a set of item ids, which you can get from character assets endpoint.
+     * Coordinates for items in hangars or stations are set to (0,0,0).
+     *
+     * @param int $characterId
+     * @param array $itemIds
+     *
+     * @return array|string
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function getCharacterAssetLocations(int $characterId, array $itemIds = [])
+    {
+        return $this->post('/characters/' . $this->encodePath($characterId) . '/assets/locations/', $itemIds);
+    }
+
+    /**
+     * Endpoint: /characters/{character_id}/assets/names/
+     *
+     * Return names for a set of item ids, which you can get from character assets endpoint.
+     * Typically used for items that can customize names, like containers or ships.
+     *
+     * @param int $characterId
+     * @param array $itemIds
+     *
+     * @return array|string
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function getCharacterAssetNames(int $characterId, array $itemIds = [])
+    {
+        return $this->post('/characters/' . $this->encodePath($characterId) . '/assets/names/', $itemIds);
+    }
+
+    /**
+     * Endpoint: /corporations/{corporation_id}/assets/
+     *
+     * Return a list of the corporation assets.
+     *
+     * @param int $corporationId
+     * @param int $page
+     *
+     * @return mixed
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function getCorporationAssets(int $corporationId, int $page = 1)
+    {
+        return $this->get('/corporations/' . $this->encodePath($corporationId) . '/assets/', $this->paginateQuery($page));
+    }
+
+    /**
+     * Endpoint: /corporations/{corporation_id}/assets/locations/
+     *
+     * Return locations for a set of item ids, which you can get from corporation assets endpoint.
+     * Coordinates for items in hangars or stations are set to (0,0,0).
+     *
+     * @param int $corporationId
+     * @param array $itemIds
+     *
+     * @return array|string
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function getCorporationAssetLocations(int $corporationId, array $itemIds = [])
+    {
+        return $this->post('/corporations/' . $this->encodePath($corporationId) . '/assets/locations/', $itemIds);
+    }
+
+    /**
+     * Endpoint: /corporations/{corporation_id}/assets/names/
+     *
+     * Return names for a set of item ids, which you can get from corporation assets endpoint.
+     * Only valid for items that can customize names, like containers or ships.
+     *
+     * @param int $corporationId
+     * @param array $itemIds
+     *
+     * @return array|string
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function getCorporationAssetNames(int $corporationId, array $itemIds = [])
+    {
+        return $this->post('/corporations/' . $this->encodePath($corporationId) . '/assets/names/', $itemIds);
+    }
 }
