917a622e6a9d9eeb0bc8409fe2f960b8fe2b0b56
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-18 16:01:33 +0000

    Add bookmarks api endpoints

--- src/Api/Bookmarks.php
+++ src/Api/Bookmarks.php
@@ -9,5 +9,71 @@ namespace AGrimes94\Esi\Api;
  */
 class Bookmarks extends AbstractApi
 {
+    /**
+     * Endpoint: /characters/{character_id}/bookmarks/
+     *
+     * A list of your character's personal bookmarks.
+     *
+     * @param int $characterId
+     * @param int $page
+     *
+     * @return mixed
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function listCharacterBookmarks(int $characterId, int $page = 1)
+    {
+        return $this->get('/characters/' . $this->encodePath($characterId) . '/bookmarks/', $this->paginateQuery($page));
+    }
 
+    /**
+     * Endpoint: /characters/{character_id}/bookmarks/folders/
+     *
+     * A list of your character's personal bookmark folders.
+     *
+     * @param int $characterId
+     * @param int $page
+     *
+     * @return mixed
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function listCharacterBookmarkFolders(int $characterId, int $page = 1)
+    {
+        return $this->get('/characters/' . $this->encodePath($characterId) . '/bookmarks/folders/', $this->paginateQuery($page));
+    }
+
+    /**
+     * Endpoint: /corporations/{corporation_id}/bookmarks/
+     *
+     * A list of your corporation's bookmarks.
+     *
+     * @param int $corporationId
+     * @param int $page
+     *
+     * @return mixed
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function listCorporationBookmarks(int $corporationId, int $page = 1)
+    {
+        return $this->get('/corporations/' . $this->encodePath($corporationId) . '/bookmarks/', $this->paginateQuery($page));
+    }
+
+    /**
+     * Endpoint: /corporations/{corporation_id}/bookmarks/folders/
+     *
+     * A list of your corporation's bookmark folders.
+     *
+     * @param int $corporationId
+     * @param int $page
+     *
+     * @return mixed
+     *
+     * @throws \Http\Client\Exception
+     */
+    public function listCorporationBookmarkFolders(int $corporationId, int $page = 1)
+    {
+        return $this->get('/corporations/' . $this->encodePath($corporationId) . '/bookmarks/folders/', $this->paginateQuery($page));
+    }
 }
