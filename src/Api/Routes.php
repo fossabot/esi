5cff2d36fe94ff5491aed588ef58bfeea02c6a6e
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-19 20:23:49 +0000

    Add routes api endpoints

--- src/Api/Routes.php
+++ src/Api/Routes.php
@@ -9,5 +9,34 @@ namespace AGrimes94\Esi\Api;
  */
 class Routes extends AbstractApi
 {
+    /**
+     * Endpoint: /route/{origin}/{destination}/
+     * HTTP Method: GET
+     *
+     * Get the systems between origin and destination.
+     *
+     * @param int $originId
+     * @param int $destinationId
+     * @param string $flag 'shortest', 'secure', 'insecure'
+     * @param array|null $avoid [30002771, 30002770, 30002769]
+     * @param array|null $connections [30002771, 30002770, 30002769]
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getRoute(int $originId, int $destinationId, string $flag = 'shortest', array $avoid = null, array $connections = null)
+    {
+        $params = [
+            'flag' => $flag,
+        ];
 
+        if (!is_null($avoid)) {
+            $params['avoid'] = $avoid;
+        }
+
+        if (!is_null($connections)) {
+            $params['connections'] = $connections;
+        }
+
+        return $this->get('/route/' . $this->encodePath($originId) . '/' . $this->encodePath($destinationId) . '/', $params);
+    }
 }
