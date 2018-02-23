ee92a13fcd774a3ccd6004259c287185a9fdfc0c
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-23 21:33:18 +0000

    Add opportunities api endpoints

--- src/Api/Opportunities.php
+++ src/Api/Opportunities.php
@@ -9,5 +9,86 @@ namespace AGrimes94\Esi\Api;
  */
 class Opportunities extends AbstractApi
 {
+    /**
+     * Endpoint: /characters/{character_id}/opportunities/
+     *
+     * HTTP Method: GET
+     *
+     * Return a list of tasks finished by a character.
+     *
+     * @param int $characterId
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getCompletedTasks(int $characterId)
+    {
+        return $this->get('/characters/' . $this->encodePath($characterId) . '/opportunities/');
+    }
 
+    /**
+     * Endpoint: /opportunities/groups/
+     *
+     * HTTP Method: GET
+     *
+     * Return a list of opportunities groups.
+     *
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getGroups()
+    {
+        return $this->get('/opportunities/groups/');
+    }
+
+    /**
+     * Endpoint: /opportunities/groups/{group_id}/
+     *
+     * HTTP Method: GET
+     *
+     * Return information of an opportunities group.
+     *
+     * @param int $groupId
+     * @param string $language
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getGroup(int $groupId, string $language = 'en-us')
+    {
+        $params = [];
+        if (!is_null($language)) {
+            $params['language'] = $language;
+        }
+        return $this->get('/opportunities/groups/' . $this->encodePath($groupId) . '/', $params);
+    }
+
+    /**
+     * Endpoint: /opportunities/tasks/
+     *
+     * HTTP Method: GET
+     *
+     * Return a list of opportunities tasks.
+     *
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getTasks()
+    {
+        return $this->get('/opportunities/tasks/');
+    }
+
+    /**
+     * Endpoint: /opportunities/tasks/{task_id}/
+     *
+     * HTTP Method: GET
+     *
+     * Return information of an opportunities task.
+     *
+     * @param int $taskId
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getTask(int $taskId)
+    {
+        return $this->get('/opportunities/tasks/' . $this->encodePath($taskId) . '/');
+    }
 }
