17a619d6972f9908f04a2c1eeb8564b2351a7794
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-23 18:48:17 +0000

    Add sov api endpoints

--- src/Api/Sovereignty.php
+++ src/Api/Sovereignty.php
@@ -9,5 +9,48 @@ namespace AGrimes94\Esi\Api;
  */
 class Sovereignty extends AbstractApi
 {
+    /**
+     * Endpoint: /sovereignty/campaigns/
+     *
+     * HTTP Method: GET
+     *
+     * Shows sovereignty data for campaigns.
+     *
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getSovereigntyCampaigns()
+    {
+        return $this->get('/sovereignty/campaigns/');
+    }
 
+    /**
+     * Endpoint: /sovereignty/map/
+     *
+     * HTTP Method :GET
+     *
+     * Shows sovereignty information for solar systems.
+     *
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getSovereigntySystems()
+    {
+        return $this->get('/sovereignty/map/');
+    }
+
+    /**
+     * Endpoint: /sovereignty/structures/
+     *
+     * HTTP Method: GET
+     *
+     * Shows sovereignty data for structures.
+     *
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function getSovereigntyStructures()
+    {
+        return $this->get('/sovereignty/structures/');
+    }
 }
