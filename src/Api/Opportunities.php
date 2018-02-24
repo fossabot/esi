6bbe8d9eb5e8b040e9caccd6b98001db5a4fe0b1
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-24 21:25:38 +0000

    Remove language params in opportunities endpoints

--- src/Api/Opportunities.php
+++ src/Api/Opportunities.php
@@ -51,20 +51,14 @@ class Opportunities extends AbstractApi
      * Return information of an opportunities group.
      *
      * @param int    $groupId
-     * @param string $language
      *
      * @throws \Http\Client\Exception
      *
      * @return mixed
      */
-    public function getGroup(int $groupId, string $language = 'en-us')
+    public function getGroup(int $groupId)
     {
-        $params = [];
-        if (!is_null($language)) {
-            $params['language'] = $language;
-        }
-
-        return $this->get('/opportunities/groups/' . $this->encodePath($groupId) . '/', $params);
+        return $this->get('/opportunities/groups/' . $this->encodePath($groupId) . '/');
     }
 
     /**
