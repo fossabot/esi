56187cc93889953a1285ed6a7740080ef019b807
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-02-25 15:24:24 +0000

    Add EsiClientTest tests

---
+++ tests/EsiClientTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AGrimes94\Esi\Tests;
+
+use Http\Client\Common\HttpMethodsClient;
+use PHPUnit\Framework\TestCase;
+use AGrimes94\Esi\EsiClient;
+use Http\Mock\Client;
+
+class EsiClientTest extends TestCase
+{
+    /**
+     * Test that a client isn't required to be passed to constructor.
+     */
+    public function testDoNotHaveToPassHttpClientToConstructor()
+    {
+        $esiClient = EsiClient::create();
+        self::assertInstanceOf(HttpMethodsClient::class, $esiClient->getHttpClient());
+    }
+
+    /**
+     * Test that passing a client to the constructor is set correctly.
+     */
+    public function testPassHttpClientToConstructor()
+    {
+        $mockClient = new Client();
+        $client = EsiClient::createWithHttpClient($mockClient);
+
+        $this->assertInstanceOf(HttpMethodsClient::class, $client->getHttpClient());
+    }
+}
