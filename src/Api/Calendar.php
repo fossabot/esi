a120303d7e18ed1b27113af65a7cd03f5b66ea8d
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-13 13:17:43 +0000

    Fix removed exceptions as throw tags in docblock in calendar api

--- src/Api/Calendar.php
+++ src/Api/Calendar.php
@@ -86,11 +86,6 @@ class Calendar extends AbstractApi
      * @param int $characterId
      * @param int $eventId
      *
-     * @throws \AGrimes94\Esi\Exception\ForbiddenResourceException
-     * @throws \AGrimes94\Esi\Exception\ResourceNotFoundException
-     * @throws \AGrimes94\Esi\Exception\ServerErrorException
-     * @throws \AGrimes94\Esi\Exception\TooManyRequestsException
-     * @throws \Exception
      * @throws \Http\Client\Exception
      *
      * @return \stdClass
